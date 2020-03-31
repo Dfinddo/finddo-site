c697cb856dfbffc8426fc12bf1d6ab16bea19b8b
Matheus Perrut <matheus.perrut2302@gmail.com>
2020-03-31 20:54:31 -0300

    - Adição da configuração de environment de desenvolvimento - Página de obtenção do token

---
+++ src/environments/environment.desenvolvimento.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://finddo-back-dev.herokuapp.com',
+  moipUrl: 'https://connect-sandbox.moip.com.br',
+  moip_client_id: 'APP-3YFUT9M9DULJ',
+  moip_client_secret: 'de91ceee0cff402fa712dcb8b6d76ac7',
+  moip_redirect_uri: 'https://perrut.github.io/finddo-front-adm/redirect',
+  moip_bearer_token: 'Bearer ed48a145a2a94bc3b5ecdf657eaf3554_v2'
+};
